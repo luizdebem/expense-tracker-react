00b762b85a61e6dd30c45675efb7442122052715
Luiz Guilherme de Bem <luizdebem99@gmail.com>
2020-05-20 19:56:38 -0300

    Balance UI little changes

--- src/components/Balance.js
+++ src/components/Balance.js
@@ -7,12 +7,13 @@ export const Balance = () => {
 
   const amounts = transactions.map(transaction => transaction.amount);
   const total = amounts.reduce((acc, item) => (acc += item), 0);
-  const sign = total < 0 ? '-' : '+';
+  const isPositive = total >= 0;
+  const sign = isPositive ? null : '-';
 
   return (
     <div className={loading ? 'your-balance loading' : 'your-balance'}>
       <h4>Your Balance:</h4>
-      <h1>{sign}{currencyFormat(Math.abs(total))}</h1>
+      <h1 className={isPositive ? 'plus' : 'minus'}>{sign} {currencyFormat(Math.abs(total))}</h1>
     </div>
   )
 }
