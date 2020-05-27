dd6c9b2aec9be5cdbc9df672f11b0425b08b2205
Luiz Guilherme de Bem <luizdebem99@gmail.com>
2020-05-26 21:14:00 -0300

    Loading Spinner on Warning Card

--- src/components/warnings/NoTransactions.js
+++ src/components/warnings/NoTransactions.js
@@ -1,11 +1,23 @@
-import React from 'react'
+import React, { useContext } from 'react'
 import PriorityHighOutlinedIcon from '@material-ui/icons/PriorityHighOutlined';
+import ClipLoader from 'react-spinners/ClipLoader';
+
+import { GlobalContext } from '../../context/GlobalState';
 
 export const NoTransactions = () => {
+
+  const { loading } = useContext(GlobalContext);
+
   return (
     <div className="no-transactions">
-      <PriorityHighOutlinedIcon/>
-      <span>No transactions. Add one below!</span>
+      {loading ?
+        <ClipLoader css="margin: 0 auto" />
+        :
+        <>
+          <PriorityHighOutlinedIcon />
+          <span>No transactions. Add one below!</span>
+        </>
+      }
     </div>
   )
 }
